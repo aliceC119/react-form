916c26dafdcad88a1657171bda7bd34688fdd7c1
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-11 13:14:09 +0000

    created a form in React & it is so far controlledby React but the submission of the form is still handled by the DOM.

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import ControlledForm from "./components/ControlledForm";
+
+function App() {
+  return <div className="App">
+      <ControlledForm />
+  </div>;
+}
+
+export default App;
